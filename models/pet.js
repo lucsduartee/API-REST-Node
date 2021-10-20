97463288e6107a370a49d0335df4687cf36c3c97
lucsduartee <lucs.rkt@gmail.com>
2021-10-20 14:11:51 -0300

    feat(model): created pet model

--- models/pet.js
+++ models/pet.js
@@ -1,15 +1,28 @@
 const conexao = require('../infra/conexao');
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos');
 
 class Pet {
   adiciona(pet, res) {
-    const sql = 'INSERT INTO Pets SET ?';
-    conexao.query(sql, pet, (err) => {
-      if (err) {
-        res.status(400).json(err);
+    const query = 'INSERT INTO Pets SET ?';
+  
+    uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+      if (erro) {
+        res.status(400).json(erro);
       } else {
-        res.status(200).json(pet);
+        const novoPet = {
+          nome: pet.nome,
+          imagem: novoCaminho,
+        };
+  
+        conexao.query(query, novoPet, (err) => {
+          if (err) {
+            res.status(400).json(err);
+          } else {
+            res.status(200).json(novoPet);
+          }
+        });
       }
-    })
+    });
   }
 }
 
